82709369dd88bac8dedf560f11af2dddb5e62235
Zul Hilmi <netspytux@gmail.com>
2020-08-08 00:54:06 +0700

    remove dist folder

--- dist/js/about.a8e4d609.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,s,a){"use strict";a.r(s);var n=function(){var t=this,s=t.$createElement;t._self._c;return t._m(0)},e=[function(){var t=this,s=t.$createElement,a=t._self._c||s;return a("div",{staticClass:"about"},[a("div",{staticClass:"container"},[a("div",{staticClass:"columns"},[a("div",{staticClass:"column"},[a("h1",{staticClass:"title"},[t._v("Hay, I'm Zul")])])])])])}],l=a("2877"),c={},i=Object(l["a"])(c,n,e,!1,null,null,null);s["default"]=i.exports}}]);
-//# sourceMappingURL=about.a8e4d609.js.map
