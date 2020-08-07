d62412c59709d136245893d3d628f72b015265de
Zul Hilmi <netspytux@gmail.com>
2020-08-08 00:26:34 +0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -10,16 +10,16 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+echo 'vue.zulhilmi.id' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:HilmiZul/hilmizul.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:HilmiZul/vue-demo.git master:gh-pages
 
 cd -
